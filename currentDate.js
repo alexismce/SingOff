5aca9e29a0ab121d3400490beedd9b79fb1f7b0b
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-15 09:55:40 -0800

    Delete currentDate.js

--- currentDate.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const dateInput = document.getElementById('date');
-  const today = new Date().toISOString().split('T')[0];
-  dateInput.value = today;
-});
