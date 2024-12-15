a9eb8bc24b9ae892e25be80fd4b70f8af3fe26f5
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-15 09:57:51 -0800

    Delete signature.js

--- signature.js
+++
@@ -1,7 +0,0 @@
-const signaturePad = new SignaturePad(document.getElementById('signature-pad'));
-const calfireSignaturePad = new SignaturePad(document.getElementById('calfire-signature-pad'));
-
-document.getElementById('clear').addEventListener('click', () => {
-  signaturePad.clear();
-  calfireSignaturePad.clear();
-});
