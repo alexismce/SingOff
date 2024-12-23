7e635f7428d7e91e4bedae788e821a31e30c9dda
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-23 02:05:40 -0800

    Update db_connect.php

--- Includes/db_connect.php
+++ Includes/db_connect.php
@@ -1,26 +1,11 @@
 <?php
-require_once '../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-if (!$dotenv->load()) {
-    die("Failed to load environment variables.");
-}
-
 $servername = getenv('DB_SERVERNAME');
-$username = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
+$dbusername = getenv('DB_USERNAME');
+$dbpassword = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
-$port = getenv('DB_PORT');
 
-// Check if environment variables are set
-if (!$servername || !$username || !$password || !$dbname || !$port) {
-    die("Environment variables for database connection are not set.");
-}
-
-try {
-    $conn = new PDO("sqlsrv:server = tcp:$servername,$port; Database = $dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error connecting to SQL Server: " . $e->getMessage());
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
