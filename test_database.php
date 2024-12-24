755c9b51917df33bffee2cb67eb767077a543fd6
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-23 22:55:13 -0800

    Update test_database.php

--- test_database.php
+++ test_database.php
@@ -1,1 +1,15 @@
+<?php
+$servername = "alexisserver.database.windows.net";
+$username = "alexismce";
+$password = "Giselle4me520@@";
+$dbname = "calfire_installs";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+$conn->close();
+?>
 
