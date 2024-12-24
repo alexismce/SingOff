5dca49deba746e37fae5986e4652beb9d2abd7ca
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-23 23:19:01 -0800

    Create db_connect_pdo.php

---
+++ db_connect_pdo.php
@@ -0,0 +1,12 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:alexisserver.database.windows.net,1433; Database = calfire_installs", "alexismce", "Giselle4me520@@");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to SQL Server successfully!";
+}
+catch (PDOException $e) {
+    echo "Error connecting to SQL Server.";
+    die(print_r($e));
+}
+?>
