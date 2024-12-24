2f263d46a58719de59f96dda3038d4ec0274343e
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-24 01:58:33 -0800

    Update db_connect.php

--- Includes/db_connect.php
+++ Includes/db_connect.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $servername = "calfi-server.mysql.database.azure.com";
 $username = "alexismce";
 $password = "Giselle4me520@@";
