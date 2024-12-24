31013b61a59f75049bd32b3ed2bdc9bc83764e96
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-23 23:21:14 -0800

    Update db_connect_pdo.php

--- db_connect_pdo.php
+++ db_connect_pdo.php
@@ -1,12 +1,10 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
+echo "<?php
 try {
-    $conn = new PDO("sqlsrv:server = tcp:alexisserver.database.windows.net,1433; Database = calfire_installs", "alexismce", "Giselle4me520@@");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to SQL Server successfully!";
+    \$conn = new PDO('sqlsrv:server = tcp:alexisserver.database.windows.net,1433; Database = calfire_installs', 'alexismce', 'Giselle4me520@@');
+    \$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Connected to SQL Server successfully!';
+} catch (PDOException \$e) {
+    echo 'Error connecting to SQL Server.';
+    die(print_r(\$e));
 }
-catch (PDOException $e) {
-    echo "Error connecting to SQL Server.";
-    die(print_r($e));
-}
-?>
+?>" > db_connect_pdo.php
