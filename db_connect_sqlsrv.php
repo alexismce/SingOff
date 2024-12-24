950a50be236e70870a01a11146efc0c32a73dd27
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-23 23:22:04 -0800

    Update db_connect_sqlsrv.php

--- db_connect_sqlsrv.php
+++ db_connect_sqlsrv.php
@@ -1,20 +1,19 @@
-<?php
-// SQL Server Extension Sample Code:
-$connectionInfo = array(
-    "UID" => "alexismce",
-    "pwd" => "Giselle4me520@@",
-    "Database" => "calfire_installs",
-    "LoginTimeout" => 30,
-    "Encrypt" => 1,
-    "TrustServerCertificate" => 0
+echo "<?php
+\$connectionInfo = array(
+    'UID' => 'alexismce',
+    'pwd' => 'Giselle4me520@@',
+    'Database' => 'calfire_installs',
+    'LoginTimeout' => 30,
+    'Encrypt' => 1,
+    'TrustServerCertificate' => 0
 );
-$serverName = "tcp:alexisserver.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+\$serverName = 'tcp:alexisserver.database.windows.net,1433';
+\$conn = sqlsrv_connect(\$serverName, \$connectionInfo);
 
-if ($conn) {
-    echo "Connected to SQL Server successfully!";
+if (\$conn) {
+    echo 'Connected to SQL Server successfully!';
 } else {
-    echo "Connection failed.";
+    echo 'Connection failed.';
     die(print_r(sqlsrv_errors(), true));
 }
-?>
+?>" > db_connect_sqlsrv.php
