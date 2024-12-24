e5ef980b67288e336d248e4a9c3456d32bf76167
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-24 00:05:53 -0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $dsn = 'odbc:Driver={ODBC Driver 18 for SQL Server};Server=tcp:alexisserver.database.windows.net,1433;Database=calfire_installs;Uid=alexismce;Pwd=Giselle4me520@@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30';
-$username = 'alexismce';
-$password = 'Giselle4me520@@';
 
 try {
-    $dbh = new PDO($dsn, $username, $password);
+    $dbh = new PDO($dsn);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected to database successfully!";
 } catch (PDOException $e) {
