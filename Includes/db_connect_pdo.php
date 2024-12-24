fae83e02bbf98dc067c0c3f006892503cff3f8aa
Alexis Felix <99692382+alexismce@users.noreply.github.com>
2024-12-24 00:54:13 -0800

    Update db_connect_pdo.php

--- Includes/db_connect_pdo.php
+++ Includes/db_connect_pdo.php
@@ -1,10 +1,15 @@
-echo "<?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$dsn = 'odbc:Driver={ODBC Driver 18 for SQL Server};Server=tcp:alexisserver.database.windows.net,1433;Database=calfire_installs;Uid=alexismce;Pwd=Giselle4me520@@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30';
+
 try {
-    \$conn = new PDO('sqlsrv:server = tcp:alexisserver.database.windows.net,1433; Database = calfire_installs', 'alexismce', 'Giselle4me520@@');
-    \$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'Connected to SQL Server successfully!';
-} catch (PDOException \$e) {
-    echo 'Error connecting to SQL Server.';
-    die(print_r(\$e));
+    $dbh = new PDO($dsn);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to database successfully!";
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
 }
-?>" > db_connect_pdo.php
+?>
